a7f88440d983eaaeae9beef0dae6f29bbab51867
Donizete Vida <donizetevida1998@outlook.com>
2022-09-02 00:17:38 -0300

    Raw pointer cast example

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+typedef struct {
+    int vertices;
+} Shape;
+
+typedef struct {
+    Shape shape;
+    int faces;
+} Square;
+
 int main(int argc, char* argv[]) {
-    printf("Hello World");
+    printf("Hello World\n");
+    Square square = {
+        .shape = {
+            .vertices = 8
+        },
+        .faces = 6
+    };
+    Shape* shape = &square;
+    int* vertices = &square;
+    printf("Vertices: %d\n", *vertices);
+    printf("Vertices: %d\n", shape->vertices);
 }
